a06b74cb2b30c7451a105c72799f0fe61f336423
ALEXUCSM <dorian.pari@ucsm.edu.pe>
2024-08-17 10:50:42 -0500

    Update Ejer4.java

--- S1 F1/EJERCICIOS PROPUESTOS/Ejer4.java
+++ S1 F1/EJERCICIOS PROPUESTOS/Ejer4.java
@@ -1,14 +1,18 @@
 package S1 F1.EJERCICIOS PROPUESTOS;
 
 public class Ejer4 {
-    
+
     public static double menor(double num1, double num2, double num3) {
-        return Math.min(num1, Math.min(num2, num3));
+        double menor = num1;
+        if (num2 < menor) menor = num2;
+        if (num3 < menor) menor = num3;
+        return menor;
     }
+
+    public static void main(String[] args) {
         double numero1 = 5.7;
         double numero2 = 3.4;
         double numero3 = 8.2;
         System.out.println("El menor número es: " + menor(numero1, numero2, numero3));
     }
 }
-
