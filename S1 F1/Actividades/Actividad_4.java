bafae89d5013b81350e0c8e9f52c185e77a92eaf
ALEXUCSM <dorian.pari@ucsm.edu.pe>
2024-08-17 10:44:09 -0500

    Update Actividad_4.java

--- S1 F1/Actividades/Actividad_4.java
+++ S1 F1/Actividades/Actividad_4.java
@@ -2,12 +2,9 @@ package S1 F1.Actividades;
 
 public class Actividad_4 {
     
-public class MenorNumero {
-
     public static double menor(double num1, double num2, double num3) {
         return Math.min(num1, Math.min(num2, num3));
     }
-
     public static void main(String[] args) {
         double numero1 = 5.7;
         double numero2 = 3.4;
