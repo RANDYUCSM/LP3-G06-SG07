908df5bbdd7adb0887e8194e2885fec56c9fe1cf
RANDYUCSM <randy.llerena@ucsm.edu.pe>
2024-08-18 10:58:32 -0500

    y

--- S1 F1/EJERCICIOS PROPUESTOS/Ejer6.java
+++ S1 F1/EJERCICIOS PROPUESTOS/Ejer6.java
@@ -1,5 +1,18 @@
-package S1 F1.EJERCICIOS PROPUESTOS;
+import java.util.Scanner;
 
-public class Ejer6 {
-    
+public class Ejer6 {  //ConvertidorTiempoASegundos
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el número de horas: ");
+        int horas = scanner.nextInt();
+        System.out.print("Ingrese el número de minutos: ");
+        int minutos = scanner.nextInt();
+        System.out.print("Ingrese el número de segundos: ");
+        int segundos = scanner.nextInt();
+        // Convertir todo a segundos
+        int totalSegundos = (horas * 3600) + (minutos * 60) + segundos;
+        System.out.println("El equivalente en segundos es: " + totalSegundos);
+        
+        scanner.close();
+    }
 }
